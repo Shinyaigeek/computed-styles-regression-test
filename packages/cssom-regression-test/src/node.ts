ca3fd4daa7a85c95bc09f5e203d942702e9ccd18
Shinyaigeek <Philispaxil@gmail.com>
2025-06-11 19:08:20 +0900

    Implement computed styles traverser from node id

--- packages/cssom-regression-test/src/node.ts
+++ packages/cssom-regression-test/src/node.ts
@@ -1,7 +1,34 @@
-export interface NodeStyle {
+import { createOk, unwrapOk, isErr, type Result } from 'option-t/esm/plain_result'
+import type { ObjectModelTraverser } from './object-model-traverser/object-model-traverser'
+
+export type CSSOMStyleValue = Record<string, string>
+
+export interface CSSOMElementNode {
+  tagName: string
+  selector: string
+  uniqueSelector: string
+  computedStyles: CSSOMStyleValue[]
+  children: CSSOMElementNode[]
+  attributes: Record<string, string>
+  textContent?: string
+}
+
+export async function getComputedStyles(
+  traverser: ObjectModelTraverser,
   nodeId: number
-  nodeName: string
-  attributes: string[]
-  computedStyle: Record<string, string>
-  children: Array<NodeStyle>
+): Promise<Result<CSSOMStyleValue, Error>> {
+  const computedStyleResult = await traverser.getComputedStyleForNode(nodeId)
+
+  if (isErr(computedStyleResult)) {
+    return computedStyleResult
+  }
+
+  const { computedStyle } = unwrapOk(computedStyleResult)
+
+  const styles = computedStyle.reduce((acc, style) => {
+    acc[style.name] = style.value
+    return acc
+  }, {} as CSSOMStyleValue)
+
+  return createOk(styles)
 }
